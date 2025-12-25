e7e356448d8364a8a7d6b76d93b500147344add9
paytonwu90 <sw9008k@gmail.com>
2025-12-25 22:20:47 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useState, useEffect, useRef } from 'react'
+import axios from 'axios'
+import { Modal } from 'bootstrap'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const { VITE_APP_PATH } = import.meta.env;
+
+function App() {
+  const [count, setCount] = useState(0);
+  const modalRef = useRef(null);
+  const bsModal = useRef(null);
+
+  useEffect(() => {
+    bsModal.current = new Modal(modalRef.current);
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button className="btn btn-primary" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <div>
+        <button type="button" className="btn btn-primary" onClick={() => bsModal.current.show()}>
+          Launch demo modal
+        </button>
+        <div className="modal fade" ref={modalRef} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                ...
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
