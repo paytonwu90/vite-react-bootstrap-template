d578c3c968b618c530aa164d6122bcb128a7e57c
paytonwu90 <sw9008k@gmail.com>
2025-12-26 04:08:51 +0800

    remove bootstrap import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import * as bootstrap from 'bootstrap'
 import './assets/all.scss'
 
 createRoot(document.getElementById('root')).render(
