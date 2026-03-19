1ce24f1e5753eebfc0a0f7a635645334c9e5f09f
paytonwu90 <sw9008k@gmail.com>
2026-03-19 22:07:14 +0800

    refactor: 優化 vite.config.js 環境判斷與路徑配置
    - 將環境判斷方式由 process 替換為 mode 參數，以修正 ESLint 警告
    - 新增 resolve.alias 配置以優化模組路徑解析

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// 取得 __dirname 替代
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/vite-react-bootstrap-template/' : '/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/vite-react-bootstrap-template/' : '/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
@@ -12,4 +24,5 @@ export default defineConfig({
       }
     }
   }
-})
+}));
+
